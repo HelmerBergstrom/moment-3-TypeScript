d320113b7a88bca507af1c0ac9a905e2f8352f9e
Helmer Bergström <hebe2400@student.miun.se>
2025-05-05 13:33:05 +0200

    Skapar komponenter för konverteringssidorna temperatur och längd. Stilar om dessa sidor i generella CSS-filen, då dessa ska se likadana ut på varje konverteringssida.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ConvertComponent } from './pages/convert/convert.component';
 import { AboutComponent } from './pages/about/about.component';
+import { TempComponent } from './pages/convert/temp/temp.component';
+import { LengthComponent } from './pages/convert/length/length.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'convert', component: ConvertComponent },
     { path: 'about', component: AboutComponent },
+    { path: 'temp', component: TempComponent},
+    { path: 'length', component: LengthComponent},
     { path: "", redirectTo: "home", pathMatch: "full"}
 ];
---
+++ src/app/pages/convert/length/length.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-length',
+  imports: [RouterLink],
+  templateUrl: './length.component.html',
+  styleUrl: './length.component.css'
+})
+export class LengthComponent {
+
+}
